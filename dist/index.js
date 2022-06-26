dff19828339269b70894cf5b5e675d32e2332256
iginfantes <iginfantes@minsait.com>
2022-06-26 18:44:13 +0200

    primera versión de introducción creación de modulos con ts

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.holaAlCurosNPM = exports.holaPersonalizado = exports.hola = void 0;
+function hola() {
+    console.log('Hola Mundo');
+}
+exports.hola = hola;
+function holaPersonalizado(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+exports.holaPersonalizado = holaPersonalizado;
+function holaAlCurosNPM() {
+    console.log('Hola al curso NPM!!!');
+}
+exports.holaAlCurosNPM = holaAlCurosNPM;
---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+export function hola(){
+    console.log('Hola Mundo');
+}
+
+export function holaPersonalizado(nombre:string){
+    console.log(`Hola ${ nombre }`);
+}
+
+export function holaAlCurosNPM(){
+    console.log('Hola al curso NPM!!!');
+}
